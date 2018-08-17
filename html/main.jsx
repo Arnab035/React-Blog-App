17dc6bd81705cfb33e50776fe7cb8cc291befd21
Arnabjyoti Kalita <arnabjyotikalita35@gmail.com>
2018-08-17 04:16:05 -0400

    Added sign-in handling but cannot see the alert

--- html/main.jsx
+++ html/main.jsx
@@ -1,20 +1,41 @@
 
 class Signin extends React.Component {
-	render() { 
-		return (
-			<form className="signin">
-				<h3 className="signin-heading">Sign In</h3>
-				<label for="inputEmail" className="ema-pass">Email Address</label>
-				<input type="email" id="inputEmail" className="form-control" placeholder="example@xyz.com" required autocomplete autofocus />
-				<label for="inputPassword" className="ema-pass">Password</label>
-				<input type="password" id="inputPassword" className="form-control" required />
-				<button className="btn btn-lg btn-primary btn-block" type="button">Sign In</button>
-		        </form>
-			);
+  constructor(props){
+    super(props);
+    this.state={
+      email:'',
+      password:''
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  
+  }
+  
+  handleEmailChange(e) {
+    this.setState({email: e.target.value});    
+  }
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});  
+  }
+
+  signIn() {
+    alert("Email address is " + this.state.email + " and password is " + this.state.password);
+  }
+
+  render() { 
+    return (
+      <form className="signin">
+	<h3 className="signin-heading">Sign In</h3>
+	<label for="inputEmail" className="ema-pass">Email Address</label>
+	<input type="email" id="inputEmail" onChange={this.handleEmailChange} className="form-control" placeholder="example@xyz.com" required autocomplete autofocus />
+	<label for="inputPassword" className="ema-pass">Password</label>
+	<input type="password" id="inputPassword" onChange={this.handlePasswordChange} className="form-control" required />
+	<button className="btn btn-lg btn-primary btn-block" type="button" onClick={this.signIn}>Sign In</button>
+      </form>
+    );
 	
-	}
-
-
+  }
 }
 
 ReactDOM.render(<Signin />, document.getElementById('app'));
