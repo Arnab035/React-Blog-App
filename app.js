02a444c5ae74a2a1c328f9b2c6af6f0eff149cd3
Arnabjyoti Kalita <arnabjyotikalita35@gmail.com>
2018-08-18 05:02:48 -0400

    Bodyparser helps reading in json data from server

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require("express");
 var path = require("path");
 
+var bodyParser = require("body-parser");
+
 var app = express();
 
 app.use(express.static(path.join(__dirname, "/html")));
 
+app.use(bodyParser.json());
+
 app.listen(9000, function(){
     console.log("Blog Server Started listening on port 9000");
+});
+
+app.post('/signin', function(request, response){
+    var username = request.body.email;
+    var password = request.body.password;
+    if (username == "admin" && password == "admin") {
+      response.send("success");
+    } else {
+      response.send("failure"); 
+    }
 });
